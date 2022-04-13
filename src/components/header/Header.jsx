89e49779343e34d9bbe1f74705aa85745f60c1ee
Blessing <91621514+BlessingJeff@users.noreply.github.com>
2022-04-13 22:50:50 +0530

    Add files via upload

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm"></span>
+        <span className="headerTitleLg">Blog</span>
+      </div>
+      <img
+        className="headerImg"
+        src="https://images.pexels.com/photos/768474/pexels-photo-768474.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt=""
+      />
+    </div>
+  );
+}
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,49 @@
+import {axiosInstance} from "../../config";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./sidebar.css";
+
+export default function Sidebar() {
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    const getCats = async () => {
+      const res = await axiosInstance.get("/categories");
+      setCats(res.data);
+    };
+    getCats();
+  }, []);
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">Blessing</span>
+        <img
+          src="https://i.ibb.co/Hztv3Fd/my.jpg"
+          alt=""
+        />
+        <p>
+          Full Stack Developer
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          {cats.map((c) => (
+            <Link to={`/?cat=${c.name}`} className="link">
+            <li className="sidebarListItem">{c.name}</li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+      {/* <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fab fa-facebook-square"></i>
+          <i className="sidebarIcon fab fa-twitter-square"></i>
+          <i className="sidebarIcon fab fa-pinterest-square"></i>
+          <i className="sidebarIcon fab fa-instagram-square"></i>
+        </div>
+      </div> */}
+    </div>
+  );
+}
